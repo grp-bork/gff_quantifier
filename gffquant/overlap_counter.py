255b0309d1d2fc37240b147d96d11a72d5c696c0
Christian Schudoma <christian.schudoma@embl.de>
2021-06-09 16:26:00 +0200

    grab everything

--- gffquant/overlap_counter.py
+++ gffquant/overlap_counter.py
@@ -2,9 +2,10 @@ import os
 import time
 import sys
 from collections import Counter
+from itertools import product
 
-import numpy
-
+import numpy as np
+import pandas as pd
 
 """
 normalizeCounts nmethod counts sizes
@@ -22,14 +23,176 @@ normalizeCounts nmethod counts sizes
                 | otherwise = 1.0e9 / initial --- 1e6 [million fragments] * 1e3 [kilo basepairs] = 1e9
         liftIO $ forM_ [1.. VUM.length counts - 1] (VUM.unsafeModify counts (* factor))
 """
+class OverlapCounter:
+    @staticmethod
+    def _generate_counter_columns(stranded=False, feature=False):
+        transforms = ("raw", "normed") + (("scaled",) if feature else tuple())
+        base_columns = tuple(product(("unique", "ambiguous"), transforms))
+        if stranded:
+            columns = list()
+            for strand in (None, False, True):
+                for col in base_columns:
+                    columns.append(col + (strand, ))
+            return columns
+        return base_columns
+                
+    def _get_annotation_function(self, annotation_mode):
+        annotation_modes = {
+            "counts": self._iterate_counts,
+            "database": self._iterate_database,
+            "bedcounts": self._iterate_bedcounts
+        }
+        try:
+            return annotation_modes[annotation_mode]
+        except KeyError:
+             raise ValueError(f"Unknown annotation_mode {itermode}")
 
-class OverlapCounter(dict):
+    def _calculate_row(self, rid, aln, strand):
+        start, end, rev_strand = aln
+        region_length = end - start + 1
+        row = self.counts.loc[rid, :]
+        if self.strand_specific:
+            row[0] = row[6] + row[12]
+            row[3] = row[9] + row[15]
+        for i in range(1, len(self.cols), 3):
+            row[i] = row[i-1] / region_length
+        return row 
+
+    #def _distribute_feature_counts(self, bins, counts, region_annotation, total_counts, feature_count_sums):
+    def _distribute_feature_counts(self, region, features, total_counts, feature_count_sums):
+        rid, start, end, rev_strand = region
+        for feature_cat, feature_subcats in features:
+            for i, fsubcat in enumerate(feature_subcats, start=1):
+                row_key = region
+                col_key = ("unique", "raw") + ((rev_strand,) if self.strand_specific else tuple())
+                counts = self.counts.loc[row_key, col_key]
+                fcounts = np.zeros(len(self.fcols))
+                k = 0
+                for j, c in enumerate(counts, start=1):
+                    if j % 3 == 0:
+                        k += 1
+                    fcounts[k] = c
+                    k += 1 
+                try:
+                    
+                
+                    new_row, update_col = np.zeros(len(self.cols)), 0
+                    if self.strand_specific:
+                        update_col = 12 if rev_strand else 6
+                    new_row[update_col] = 1
+                    self.counts = self.counts.append(
+                        pd.DataFrame(data=[new_row], index=[row_key], columns=self.counts.columns)
+                    )
+
+
+
+
+
+
+            
+        for ftype, ftype_counts in region_annotation:
+            for i, ft_ct in enumerate(ftype_counts, start=1):
+                fcounts = self.featcounts.setdefault(ftype, dict()).setdefault(ft_ct, np.zeros(bins))
+                total_fcounts = feature_count_sums.setdefault(ftype, np.zeros(4))
+                fcounts += counts
+
+            inc = counts[:4] * i
+            total_counts += inc
+            total_fcounts += inc
+
+        return total_counts, feature_count_sums
+    
+
+
+
+
+    def _iterate_counts(self, bins, refdata):
+        total_counts, feature_count_sums = np.zeros(4), dict()
+
+        current_rid = None
+        for rid, start, end, rev_strand in self.counts.index:
+            if current_rid != rid:
+                ref, rlen = refdata.get(rid)
+            (_, strand), (_, gene_id), *features = self.db.get_data(ref, start, end)
+            counts = self._calculate_row(rid, (start, end, rev_strand), strand)
+            total_counts, feature_count_sums = self._distribute_feature_counts(bins, counts, features, total_counts, feature_count_sums)
+
+        return total_counts, feature_count_sums
+            
+    def _iterate_database(self):
+        ...
+    def _iterate_bedcounts(self):
+        ...
+
     @staticmethod
     def normalise_counts(counts, feature_len, scaling_factor):
         '''Returns raw, length-normalised, and scaled feature counts.'''
         normalised = counts / feature_len
         scaled = normalised * scaling_factor
         return counts, normalised, scaled
+
+    def __init__(self, nrefs=0, db=None, do_overlap_detection=True, strand_specific=False):
+        self.db = db
+        self.do_overlap_detection = do_overlap_detection
+        self.strand_specific = strand_specific
+        self.cols = OverlapCounter._generate_counter_columns(stranded=strand_specific)
+        self.counts = pd.DataFrame(data=None, columns=self.cols, dtype=np.float64)
+        self.seqcounts = pd.DataFrame(
+            data=[[0] * len(self.cols)] * nrefs, index=range(nrefs), columns=self.cols, dtype=np.float64
+        ) 
+        self.fcols = OverlapCounter._generate_counter_columns(stranded=strand_specific, feature=True)
+        self.featcounts = pd.DataFrame(data=None, columns=self.fcols, dtype=np.float64)
+        self.unannotated = 0
+
+    def update_unique_counts(self, rid, aln=None, rev_strand=False):
+        if aln:
+            overlaps = [0] #self.db.get_overlaps(*aln)
+            self.unannotated += min(1, len(overlaps))
+            for ovl in overlaps:
+                #row_key = (rid, ovl.begin, ovl.end, rev_strand)
+                row_key = (4, 1, 2, None)
+                col_key = ("unique", "raw") + ((rev_strand,) if self.strand_specific else tuple())
+                print(col_key)
+                try:
+                    self.counts.at[row_key, col_key] += 1
+                except KeyError:
+                    new_row, update_col = np.zeros(len(self.cols)), 0
+                    if self.strand_specific:
+                        update_col = 12 if rev_strand else 6
+                    new_row[update_col] = 1
+                    self.counts = self.counts.append(
+                        pd.DataFrame(data=[new_row], index=[row_key], columns=self.counts.columns)
+                    )
+
+        self.seqcounts.at[rid, col_key] += 1
+
+    def annotate_counts(self, refdata, annotation_mode="counts"):
+        print("Processing counts ...", flush=True)
+        t0 = time.time()
+        bins = 12 if self.strand_specific else 4
+        default_scaling_factor = 0
+
+        annotation_f = self._get_annotation_function(annotation_mode)
+        total_counts, feature_count_sums = annotation_f(bins, refdata)
+
+        # calculate the scaling factors
+        total, total_normed, total_ambi, total_ambi_normed = total_counts
+
+        for ftype, counts in feature_count_sums.items():
+            total, total_normed, total_ambi, total_ambi_normed = counts
+            self.feature_scaling_factors[ftype] = (
+                (total / total_normed) if total_normed else default_scaling_factor,
+                (total_ambi / total_ambi_normed) if total_ambi_normed else default_scaling_factor
+            )
+
+        t1 = time.time()
+        print("Processed counts in {n_seconds}s.".format(n_seconds=t1-t0), flush=True)
+
+
+
+                
+
+class xOverlapCounter(dict):
     def __init__(self, out_prefix, db, do_overlap_detection=True, strand_specific=False):
         self.out_prefix = out_prefix
         self.db = db
@@ -43,48 +206,15 @@ class OverlapCounter(dict):
         self.do_overlap_detection = do_overlap_detection
         self.strand_specific = strand_specific
 
-    def update_unique_counts(self, rid, aln=None, rev_strand=False):
-        '''
-        Generates a hit list from the overlaps resulting from an intervaltree query,
-        adds the number of alternative alignments and stores the results for each reference sequence.
-        '''
-        if aln:
-            overlaps = self.db.get_overlaps(*aln)
-            if overlaps:
-                self.setdefault(rid, Counter()).update((ovl.begin, ovl.end, rev_strand) for ovl in overlaps)
-            else:
-                self.unannotated_reads += 1
-        if self.strand_specific and not self.do_overlap_detection:
-            self.seqcounts[(rid, rev_strand)] += 1
-        else:
-            self.seqcounts[rid] += 1
-
-    def _compute_count_vector(self, bins, rid, aln, strand, strand_specific):
-        # calculate the count vector for the current region
-        # count vectors are of the format (raw_uniq, normed_uniq, raw_ambi, normed_ambi)
-        # att: _ambi counts are unique + ambiguous
-        counts = numpy.zeros(bins)
-        region_length = aln[1] - aln[0] + 1
-        counts[0] = counts[1] = self.get(rid, dict()).get(aln, 0.0)
-        counts[1] /= region_length
-        counts[2] = counts[3] = counts[0] + self.ambig_counts.get(rid, dict()).get(aln, 0.0)
-        counts[3] /= region_length
-        if strand_specific:
-            rev_strand = aln[-1]
-            is_antisense = (strand == "+" and rev_strand) or (strand == "-" and not rev_strand)
-            bin_offset = 8 if is_antisense else 4
-            counts[bin_offset:bin_offset + 4] += counts[:4]
-
-        return counts
 
     def _iterate_database(self, bins, bam, strand_specific):
-        total_counts, feature_count_sums = numpy.zeros(4), dict()
+        total_counts, feature_count_sums = np.zeros(4), dict()
 
         for ref, region_annotation in self.db.iterate():
             rid = bam.revlookup_reference(ref)
             if rid is not None:
                 _, region_length = bam.get_reference(rid)
-                counts = numpy.zeros(bins)
+                counts = np.zeros(bins)
 
                 if strand_specific:
                     counts[0] = counts[1] = self.seqcounts[(rid, True)] + self.seqcounts[(rid, False)]
@@ -111,7 +241,7 @@ class OverlapCounter(dict):
 
 
     def _iterate_bedcounts(self, bins, feature_lengths, strand_specific):
-        total_counts, feature_count_sums = numpy.zeros(4), dict()
+        total_counts, feature_count_sums = np.zeros(4), dict()
 
         for rid in set(self.seqcounts).union(self.ambig_seqcounts):
             region_length = feature_lengths.get(rid)
@@ -119,7 +249,7 @@ class OverlapCounter(dict):
 
             if region_length is None:
                 raise ValueError(f"Cannot determine length of reference {rid}.")
-            counts = numpy.zeros(bins)
+            counts = np.zeros(bins)
 
             if strand_specific:
                 counts[0] = counts[1] = self.seqcounts[(rid, True)] + self.seqcounts[(rid, False)]
@@ -145,21 +275,21 @@ class OverlapCounter(dict):
         return total_counts, feature_count_sums
 
 
-    def _distribute_feature_counts(self, bins, counts, region_annotation, total_counts, feature_count_sums):
-        for ftype, ftype_counts in region_annotation:
-            for i, ft_ct in enumerate(ftype_counts, start=1):
-                fcounts = self.featcounts.setdefault(ftype, dict()).setdefault(ft_ct, numpy.zeros(bins))
-                total_fcounts = feature_count_sums.setdefault(ftype, numpy.zeros(4))
-                fcounts += counts
 
-            inc = counts[:4] * i
-            total_counts += inc
-            total_fcounts += inc
 
-        return total_counts, feature_count_sums
+
+
+
+
+
+
+
+
+
+
 
     def _iterate_counts(self, bins, bam, strand_specific):
-        total_counts, feature_count_sums = numpy.zeros(4), dict()
+        total_counts, feature_count_sums = np.zeros(4), dict()
 
         for rid in set(self.keys()).union(self.ambig_counts):
             ref = bam.get_reference(rid)[0]
@@ -178,50 +308,6 @@ class OverlapCounter(dict):
         return total_counts, feature_count_sums
 
 
-    def annotate_counts(self, bamfile=None, feature_lengths=None, itermode="counts"):
-        """
-        Distributes read counts against aligned regions to the associated functional categories.
-        """
-        print("Processing counts ...", flush=True)
-        t0 = time.time()
-        bins = 12 if self.strand_specific else 4
-
-
-        annotation_f = {
-            "counts": self._iterate_counts,
-            "database": self._iterate_database,
-            "bedcounts": self._iterate_bedcounts
-        }.get(itermode)
-
-        if not annotation_f:
-            raise ValueError(f"Unknown annotation_mode {itermode}")
-
-        if bool(bamfile) == bool(feature_lengths):
-            raise ValueError(f"Cannot determine feature lengths.")
-        if not feature_lengths:
-            feature_lengths = bamfile
-
-        total_counts, feature_count_sums = annotation_f(bins, feature_lengths, self.strand_specific)
-
-        # calculate the scaling factors
-        total, total_normed, total_ambi, total_ambi_normed = total_counts
-
-        # are these used?
-        default_scaling_factor = 0
-        self.feature_scaling_factors["total"] = (total / total_normed) if total_normed else default_scaling_factor
-        self.feature_scaling_factors["total_ambi"] = (total_ambi / total_ambi_normed) if total_ambi_normed else default_scaling_factor
-
-        for ftype, counts in feature_count_sums.items():
-            total, total_normed, total_ambi, total_ambi_normed = counts
-            self.feature_scaling_factors[ftype] = (
-                (total / total_normed) if total_normed else default_scaling_factor,
-                (total_ambi / total_ambi_normed) if total_ambi_normed else default_scaling_factor
-            )
-
-        self.clear()
-        self.ambig_counts.clear()
-        t1 = time.time()
-        print("Processed counts in {n_seconds}s.".format(n_seconds=t1-t0), flush=True)
 
 
     def update_ambiguous_counts(self, hits, n_aln, unannotated=0, bam=None, feat_distmode="all1"):
